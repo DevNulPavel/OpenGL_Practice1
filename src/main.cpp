61745122639f588829c2e256294be852a1d4b406
Pavel Ershov <devnulpavel@gmail.com>
2016-03-13 12:43:41 +0300

    Еще правки

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 // TODO: надо ли?
 // #define GLFW_INCLUDE_GLCOREARB 1 // Tell GLFW to include the OpenGL core profile header
+#define GLFW_INCLUDE_GLU
+#define GLFW_INCLUDE_GL3
+#define GLFW_INCLUDE_GLEXT
 #include <string>
 #include <iostream>
 #include <math.h>
