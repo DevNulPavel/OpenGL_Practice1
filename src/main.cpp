8ee57aec65168cbe350c4dabdaaecd0fb7dc03ba
Pavel Ershov <devnulpavel@gmail.com>
2019-02-15 11:47:32 +0300

    Small improvement

--- src/main.cpp
+++ src/main.cpp
@@ -101,6 +101,8 @@ int main(int argc, char *argv[]) {
     //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 #else
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
